7be0707cef6151ebcbf71e048cc6d6e8f78f143d
Equilnox <m.mitew.1079@abv.bg>
2024-02-24 12:42:00 +0200

    Add project files.

---
+++ FitnessTracker/Extensions/FitnessTrackerCollectionExtension.cs
@@ -0,0 +1,34 @@
+﻿using FitnessTracker.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class FitnessTrackerCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
