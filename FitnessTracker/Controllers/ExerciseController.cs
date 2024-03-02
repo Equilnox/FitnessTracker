1d4912c17de549ce4ee22de1d56f0a081b3fe573
Equilnox <m.mitew.1079@abv.bg>
2024-03-02 18:02:10 +0200

    Added CreateExercise

--- FitnessTracker/Controllers/ExerciseController.cs
+++ FitnessTracker/Controllers/ExerciseController.cs
@@ -1,9 +1,11 @@
-﻿using FitnessTracker.Core.Models.ViewModels;
+﻿using FitnessTracker.Core.Models.Exercise;
 using FitnessTracker.Infrastructure.Data;
+using FitnessTracker.Infrastructure.Data.Models;
 using FitnessTracker.Infrastructure.Data.Models.Enums;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 namespace FitnessTracker.Controllers
 {
@@ -43,13 +45,47 @@ namespace FitnessTracker.Controllers
             return View(model);
         }
 
-        public async Task<IActionResult> Details(string name)
+        public async Task<IActionResult> Details(int id)
         {
             var exerciseList = await GetAllExercises();
 
-            var model = exerciseList.FirstOrDefault(e => e.Name == name);
+            var model = exerciseList.FirstOrDefault(e => e.Id == id);
 
             return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult CreateExercise()
+        {
+            var model = new ExerciseFormModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateExercise(ExerciseFormModel model)
+        {
+            var modelName = model.Name.Count();
+            var modelDescription = model.Description.Count();
+
+            if (!ModelState.IsValid)
+            {
+                model = new ExerciseFormModel();
+
+                return View(model);
+            }
+
+            var newExercise = new Exercise()
+            {
+                Name = model.Name,
+                Description = model.Description,
+				MuscleGroup = (MuscleGroup)Enum.Parse(typeof(MuscleGroup), model.MuscleGroup)
+            };
+
+            await data.AddAsync(newExercise);
+            await data.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Details), new { id = newExercise.Id });
         }
 
         private async Task<List<ExerciseViewModel>> GetAllExercises()
@@ -57,6 +93,7 @@ namespace FitnessTracker.Controllers
             return await data.Exercises
                             .Select(e => new ExerciseViewModel
                             {
+                                Id = e.Id,
                                 Name = e.Name,
                                 Description = e.Description,
                                 MuscleGroup = e.MuscleGroup.ToString(),
