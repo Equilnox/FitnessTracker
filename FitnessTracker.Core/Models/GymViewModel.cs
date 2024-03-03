54b7419158e0173553a949893d64d9320e8cd2c1
Equilnox <m.mitew.1079@abv.bg>
2024-03-03 12:45:12 +0200

    Added Id property to GymViewModel

--- FitnessTracker.Core/Models/GymViewModel.cs
+++ FitnessTracker.Core/Models/GymViewModel.cs
@@ -2,6 +2,8 @@
 {
     public class GymViewModel
     {
+        public int Id { get; set; }
+
         public string Name { get; set; } = string.Empty;
 
         public string Address { get; set; } = string.Empty;
