ef49075e4ffd930c4d2eba8fb495af6e4c9e8cb1
Equilnox <m.mitew.1079@abv.bg>
2024-03-04 11:05:30 +0200

    Added AllGym view

--- FitnessTracker/Controllers/GymController.cs
+++ FitnessTracker/Controllers/GymController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FitnessTracker.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FitnessTracker.Controllers
 {
 	public class GymController : Controller
 	{
-		public IActionResult All()
+		private readonly IGymService service;
+
+		public GymController(IGymService _service)
 		{
-			return View();
+			service = _service;
+		}
+
+		public async Task<IActionResult> All()
+		{
+			var model = await service.GetAllAsync();
+
+			return View(model);
 		}
 	}
 }
