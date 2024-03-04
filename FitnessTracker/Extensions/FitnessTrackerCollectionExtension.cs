e3fe1d1b1609fb10b06aa94ec3287ac0e9c415fa
Equilnox <m.mitew.1079@abv.bg>
2024-03-04 11:07:05 +0200

    Added Gym services to FitnessTrackerCollectionExtension

--- FitnessTracker/Extensions/FitnessTrackerCollectionExtension.cs
+++ FitnessTracker/Extensions/FitnessTrackerCollectionExtension.cs
@@ -13,6 +13,7 @@ namespace Microsoft.Extensions.DependencyInjection
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddScoped<IExerciseService, ExerciseService>();
+            services.AddScoped<IGymService, GymService>();
 
             return services;
         }
