0f6859269181f67ddb81206b5e41d8c2d5b0a1c4
Equilnox <m.mitew.1079@abv.bg>
2024-03-04 11:04:36 +0200

    Created GymService

---
+++ FitnessTracker.Core/Contracts/IGymService.cs
@@ -0,0 +1,17 @@
+﻿using FitnessTracker.Core.Models.Gym;
+using FitnessTracker.Infrastructure.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessTracker.Core.Contracts
+{
+    public interface IGymService
+    {
+        Task<IEnumerable<GymViewModel>> GetAllAsync();
+
+        Task<GymViewModel> GetGymAsync(int id);
+    }
+}
---
+++ FitnessTracker.Core/Services/GymService.cs
@@ -0,0 +1,44 @@
+﻿using FitnessTracker.Core.Contracts;
+using FitnessTracker.Core.Models.Gym;
+using FitnessTracker.Infrastructure.Data.Common;
+using FitnessTracker.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessTracker.Core.Services
+{
+    public class GymService : IGymService
+    {
+        private readonly IRepository repository;
+
+        public GymService (IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public async Task<IEnumerable<GymViewModel>> GetAllAsync()
+        {
+            var model = await repository.AllReadOnly<Gym>()
+                .Select(g => new GymViewModel
+                {
+                    Id = g.Id,
+                    Name = g.Name,
+                    Address = g.Address,
+                    PhoneNumber = g.PhoneNumber,
+                    PricePerMonth = g.PricePerMonth.ToString()
+                })
+                .ToListAsync();
+
+            return model;
+        }
+
+        public Task<GymViewModel> GetGymAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
