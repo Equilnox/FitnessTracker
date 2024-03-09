42ecee482f77fd4dc0e81e4044d0c71e336969e0
Equilnox <m.mitew.1079@abv.bg>
2024-03-09 11:22:24 +0200

    Added Athlete Create

---
+++ FitnessTracker.Core/Models/Athlete/AthleteCreateFormModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static FitnessTracker.Infrastructure.Data.Constrains.DataConstrains;
+using static FitnessTracker.Infrastructure.Data.Constrains.ErrorMessages;
+
+namespace FitnessTracker.Core.Models.Athlete
+{
+    public class AthleteCreateFormModel
+    {
+        [Required(ErrorMessage = RequiredFieldMessage)]
+        [StringLength(UserFirstNameMaxLength, MinimumLength = UserFirstNameMinLength, ErrorMessage = StringLengthMessage)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredFieldMessage)]
+        [StringLength(UserLastNameMaxLength, MinimumLength = UserLastNameMinLength, ErrorMessage = StringLengthMessage)]
+        public string LastName { get; set; } = string.Empty;
+    }
+}
